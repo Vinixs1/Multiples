c20cf0c5503c81fb135ce7ff1086c68a87c59110
Vinixs <bbjthill03@gmail.com>
2021-08-30 23:30:06 -0500

    - fixed item lore & meta

--- src/main/java/me/multiplesdev/multiples/items/ItemManager.java
+++ src/main/java/me/multiplesdev/multiples/items/ItemManager.java
@@ -1,5 +1,6 @@
 package me.multiplesdev.multiples.items;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
@@ -25,15 +26,16 @@ public class ItemManager {
         // Basic Shovel
         ItemStack basicShovelItem = new ItemStack(Material.GOLDEN_SHOVEL, 1);
         ItemMeta basicShovelMeta = basicShovelItem.getItemMeta();
-        basicShovelMeta.setDisplayName("&f&lBasic Shovel");
+        basicShovelMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&f&lBasic Shovel"));
         List<String> basicShovelLore = new ArrayList<>();
-        basicShovelLore.add("&bEfficiency I");
+        basicShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bEfficiency I"));
         basicShovelLore.add("");
-        basicShovelLore.add(" &8| &7Empty");
-        basicShovelLore.add("");
+        basicShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
         basicShovelMeta.setLore(basicShovelLore);
         basicShovelMeta.addEnchant(Enchantment.DIG_SPEED, 1, false);
         basicShovelMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        basicShovelMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        basicShovelMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
         basicShovelItem.setItemMeta(basicShovelMeta);
         basicShovel = basicShovelItem;
 
@@ -42,15 +44,16 @@ public class ItemManager {
         ItemMeta goodShovelMeta = goodShovelItem.getItemMeta();
         goodShovelMeta.setDisplayName("&a&lGood Shovel");
         List<String> goodShovelLore = new ArrayList<>();
-        goodShovelLore.add("&bEfficiency I");
-        goodShovelLore.add("&bFortune I");
+        goodShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bEfficiency I"));
+        goodShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bFortune I"));
         goodShovelLore.add("");
-        goodShovelLore.add(" &8| &7Empty");
-        goodShovelLore.add(" &8| &7Empty");
-        goodShovelLore.add("");
+        goodShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        goodShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
         goodShovelMeta.setLore(goodShovelLore);
         goodShovelMeta.addEnchant(Enchantment.DIG_SPEED, 1, false);
         goodShovelMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        goodShovelMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        goodShovelMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
         goodShovelItem.setItemMeta(goodShovelMeta);
         goodShovel = goodShovelItem;
 
@@ -59,16 +62,17 @@ public class ItemManager {
         ItemMeta legendaryShovelMeta = legendaryShovelItem.getItemMeta();
         legendaryShovelMeta.setDisplayName("&6&lLegendary Shovel");
         List<String> legendaryShovelLore = new ArrayList<>();
-        legendaryShovelLore.add("&bEfficiency II");
-        legendaryShovelLore.add("&bFortune II");
+        legendaryShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bEfficiency II"));
+        legendaryShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bFortune II"));
         legendaryShovelLore.add("");
-        legendaryShovelLore.add(" &8| &7Empty");
-        legendaryShovelLore.add(" &8| &7Empty");
-        legendaryShovelLore.add(" &8| &7Empty");
-        legendaryShovelLore.add("");
+        legendaryShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        legendaryShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        legendaryShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
         legendaryShovelMeta.setLore(legendaryShovelLore);
         legendaryShovelMeta.addEnchant(Enchantment.DIG_SPEED, 1, false);
         legendaryShovelMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        legendaryShovelMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        legendaryShovelMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
         legendaryShovelItem.setItemMeta(legendaryShovelMeta);
         legendaryShovel = legendaryShovelItem;
 
@@ -77,17 +81,18 @@ public class ItemManager {
         ItemMeta mythicShovelMeta = mythicShovelItem.getItemMeta();
         mythicShovelMeta.setDisplayName("&d&lMythic Shovel");
         List<String> mythicShovelLore = new ArrayList<>();
-        mythicShovelLore.add("&bEfficiency III");
-        mythicShovelLore.add("&bFortune II");
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bEfficiency III"));
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', "&bFortune II"));
         mythicShovelLore.add("");
-        mythicShovelLore.add(" &8| &7Empty");
-        mythicShovelLore.add(" &8| &7Empty");
-        mythicShovelLore.add(" &8| &7Empty");
-        mythicShovelLore.add(" &8| &7Empty");
-        mythicShovelLore.add("");
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
+        mythicShovelLore.add(ChatColor.translateAlternateColorCodes('&', " &8| &7Empty"));
         mythicShovelMeta.setLore(mythicShovelLore);
         mythicShovelMeta.addEnchant(Enchantment.DIG_SPEED, 1, false);
         mythicShovelMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        mythicShovelMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        mythicShovelMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
         mythicShovelItem.setItemMeta(mythicShovelMeta);
         mythicShovel = mythicShovelItem;
 
